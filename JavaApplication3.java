586894186caa3b0d0d9076d6684f04ca1df71057
Eduarda Gama <eduarda.gama@salg.com.br>
2018-08-22 11:00:07 -0300

    first commit

---
+++ JavaApplication3.java
@@ -0,0 +1,28 @@
+
+package javaapplication3;
+
+import java.util.Scanner;
+
+public class JavaApplication3 {
+
+
+    public static void main(String[] args) {
+        
+       Scanner ent = new Scanner(System.in);
+        int num, fat = 1;
+        int cont = 1;
+
+        do{
+            System.out.println("Digite um nº");
+            num = ent.nextInt();
+            
+            for(int i = 1;i <= num; i++){
+                fat = fat * i;
+            }
+            
+            System.out.println("!" + num + " = " + fat);
+            cont++;
+            
+       }while(cont < 2);
+    }
+}
