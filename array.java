3b736af794097b6751988f2f173044fcad8c20ac
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-18 11:51:08 -0300

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+package javaapplication10;
+import java.util.*;
+public class JavaApplication10 {
+
+    public static void main(String[] args) {
+String [] animais = {"cachorro", "passarinho", "peixe", "gato", "rato"};
+
+
+String [] pessoas = new String[2];
+pessoas[0] = "duda";
+pessoas[1] = "felipe";
+
+        System.out.println(pessoas[1]);
+        System.out.println(animais[3]);
+
+}}
