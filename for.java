dfe23703b131c3932011939df9af1238bf902de2
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-18 08:17:22 -0300

    Create for.java

---
+++ for.java
@@ -0,0 +1,13 @@
+
+package javaapplication10;
+import java.util.*;
+public class JavaApplication10 {
+
+
+    public static void main(String[] args) {
+int num = 0;
+for (num = 0; num < 10; num++){
+    System.out.println(num);
+                } 
+}    
+}
