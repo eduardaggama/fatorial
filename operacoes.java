4f6d4af8eba49ed2cb12677eabe56f39fa177a30
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-18 19:24:42 -0300

    Create operacoes.java

---
+++ operacoes.java
@@ -0,0 +1,14 @@
+
+package javaapplication10;
+public class JavaApplication10 {
+
+    public static void main(String[] args) {
+int num = 2;
+int num2 = 5;
+
+        System.out.println(num + num2);
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num / num2);
+}
+}
