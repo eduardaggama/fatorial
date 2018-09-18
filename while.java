3866495c3322b6bb0fad83a99ab04080be7b1798
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-18 08:14:45 -0300

    Create while.java

---
+++ while.java
@@ -0,0 +1,13 @@
+package javaapplication10;
+import java.util.*;
+public class JavaApplication10 {
+
+
+    public static void main(String[] args) {
+int num = 0;
+while (num < 10){
+    System.out.println("gama");
+    num = num + 1;
+                } 
+}    
+}
