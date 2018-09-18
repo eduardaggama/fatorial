445ffd53476804a58684dd19fbf10670671c8ae0
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-18 08:10:54 -0300

    Create inch.java

---
+++ inch.java
@@ -0,0 +1,20 @@
+package javaapplication10;
+import java.util.*;
+public class JavaApplication10 {
+
+    public static void main(String[] args) {
+Scanner ler = new Scanner(System.in);
+
+double inch = 0.0254;
+int num;
+double total;
+
+        System.out.println("Quantas inch?");
+        metros = ler.nextInt();
+        
+        total = num * inch;
+        
+        System.out.println(total);
+    }
+    
+}
