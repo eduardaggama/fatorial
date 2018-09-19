34f8d327d69c7ceb788e63f54e9927908eed0510
Eduarda Gama <eduarda.gama@salg.com.br>
2018-09-19 07:58:12 -0300

    Create metodos.java

---
+++ metodos.java
@@ -0,0 +1,14 @@
+package javaapplication15;
+import java.util.*;
+public class JavaApplication15 {
+    
+public static void main(String[] args) {
+adicao();
+        }
+static void adicao(){
+int a = 3;
+int b = 5;
+    System.out.println(a+b);
+
+}
+}
